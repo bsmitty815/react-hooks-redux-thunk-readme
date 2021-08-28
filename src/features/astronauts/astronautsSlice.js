c06fd08f00c0dedcf801e27c5769b1b230c32dba
Brian Smith <brian.bubbam@gmail.com>
2021-08-28 13:25:12 -0500

    commit tree 591176b8381fe94d3e882cac47bc83112ec75570

--- src/features/astronauts/astronautsSlice.js
+++ src/features/astronauts/astronautsSlice.js
@@ -1,10 +1,17 @@
 // Action Creators
-export function fetchAstronauts(astronauts) {
-  return {
-    type: "astronauts/astronautsLoaded",
-    payload: astronauts,
+export function fetchAstronauts() {
+  return function (dispatch) {
+    dispatch({ type: "astronauts/astronautsLoading" });
+    fetch("http://api.open-notify.org/astros.json")
+      .then((response) => response.json())
+      .then((astronauts) =>
+        dispatch({
+          type: "astronauts/astronautsLoaded",
+          payload: astronauts.people,
+        })
+      );
   };
-}
+} 
 
 // Reducers
 const initialState = {
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import App from "./App";
 import rootReducer from "./reducer";
+import thunkMiddleware from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
 
 const store = createStore(
   rootReducer,
